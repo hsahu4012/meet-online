23a7369ae5f36b2165032b559dc487d26c115af6
hsahu4012 <hrudayanandasahu3@gmail.com>
2024-07-15 09:15:17 +0530

    responsiveness

--- components/HomeCard.tsx
+++ components/HomeCard.tsx
@@ -16,7 +16,7 @@ const HomeCard = ({ className, img, title, description, handleClick }: HomeCardP
   return (
     <section
       className={cn(
-        'bg-orange-1 px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer',
+        'bg-orange-1 px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] xl:min-h-[260px] rounded-[14px] cursor-pointer hover:bg-blue-500 md:min-h-[260px] sm:min-h-[100px]',
         className
       )}
       onClick={handleClick}
@@ -24,7 +24,7 @@ const HomeCard = ({ className, img, title, description, handleClick }: HomeCardP
       <div className="flex-center glassmorphism size-12 rounded-[10px]">
         <Image src={img} alt="meeting" width={27} height={27} />
       </div>
-      
+
       <div className="flex flex-col gap-2">
         <h1 className="text-2xl font-bold">{title}</h1>
         <p className="text-lg font-normal">{description}</p>
